e72841d22f498288a8787f2fd6dc342856227bf9
Andre Butler <andrebutler@mmc.school.nz>
2019-07-03 10:30:37 +1200

    Added firework sprite.

---
+++ Andre Butler - 12Ho - VS - Game/Firework.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Andre_Butler___12Ho___VS___Game
+{
+    class Firework
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image fireworkImage;//variable for the planet's image
+        public Rectangle fireworkRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Firework()
+        {
+            x = 10;
+            y = 10;
+            width = 33;
+            height = 17;
+            fireworkImage = Image.FromFile("chargamevsfw.png");
+            fireworkRec = new Rectangle(x, y, width, height);
+        }
+
+        // Methods for the Planet class
+        public void drawPlanet(Graphics g)
+        {
+            g.DrawImage(fireworkImage, fireworkRec);
+        }
+
+    }
+}
--- Andre Butler - 12Ho - VS - Game/Form1.cs
+++ Andre Butler - 12Ho - VS - Game/Form1.cs
@@ -12,9 +12,28 @@ namespace Andre_Butler___12Ho___VS___Game
 {
     public partial class Form1 : Form
     {
+        Graphics g; //declare a graphics object called g
+        Firework firework = new Firework(); //create the object, planet1
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            {
+                //get the graphics used to paint on the panel control
+                g = e.Graphics;
+                //call the Planet class's DrawPlanet method to draw the image planet1 
+                firework.drawPlanet(g);
+            }
+
         }
     }
 }
